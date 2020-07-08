4387faf67d850fbdcfa7e7db935753bebcd26486
Venkatasubramanian <pvenkat.rv@gmail.com>
2020-07-08 11:31:48 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,33 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+
+const app = express();
+
+app.use(bodyParser.json());
+
+let userDetails = [
+  {
+    id: "1",
+    name: "venkat",
+    email: "venkat@guvi.in",
+  },
+];
+
+app.listen(3000, () => {
+  console.log("your app is running in 3000");
+});
+
+app.get("/", (req, res) => {
+  res.send("<h1>Hello World</h1>");
+});
+
+app.get("/users", (req, res) => {
+  res.json(userDetails);
+});
+
+app.post("/users", (req, res) => {
+  userDetails.push(req.body);
+  res.json({
+    message: "User Created..!!",
+  });
+});
