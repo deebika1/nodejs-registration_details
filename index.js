55bf9b501b2d33fa4ec6589e677c0e4cf94ca3e8
Venkatasubramanian <pvenkat.rv@gmail.com>
2020-07-10 13:18:24 +0530

    fixed cros

--- index.js
+++ index.js
@@ -1,9 +1,15 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const cors = require('cors');
 
 const app = express();
 
 app.use(bodyParser.json());
+
+app.use(cors({
+    origin: '*'
+}));
+
 
 const port = process.env.PORT || 5000;
 
