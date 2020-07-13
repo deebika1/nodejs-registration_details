636dcb50548deb993b94384e6e4b108b482bb477
Venkatasubramanian <pvenkat.rv@gmail.com>
2020-07-13 16:24:25 +0530

    install of mongodb

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const cors = require('cors');
 
+const mongodb = require('mongodb');
+
 const app = express();
 
 app.use(bodyParser.json());
